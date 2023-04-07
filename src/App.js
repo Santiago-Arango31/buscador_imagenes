a95c7edb1673ecbb375cb75b95100b821cb5d1b5
Santiago Arango <sarango@be4tech.co>
2023-04-07 10:27:35 -0500

    buscador de imagenes en reack implementando formularios y consumiendo api unsplash

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Form, Field, Formik } from "formik";
+import { useState } from "react";
+import './header.css'
+import './container.css'
+
 
 function App() {
+  const [photos, setPhotos] = useState([])
+  const open = url => window.open(url)
+  console.log({ photos })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: '' }}
+          onSubmit={
+            async values => {
+              const resp = await fetch(`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`, {
+                headers: {
+                  'Authorization': 'Client-ID fVInZfuMiOCbG2Hr4XMa5iYfXhj06g6F4S1S-wXItlA'
+                }
+              })
+              const data = await resp.json()
+              setPhotos(data.results)
+
+            }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name='search'></Field>
+          </Form>
+        </Formik>
       </header>
+      <div className="container">
+        <div className="center">
+          {photos.map(photo =>
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} />
+              <p>{[photo.description, photo.alt_description].join('-')}</p>
+            </article>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
